0c5883bfc59e2a09b3906bfaf296f2bd1210c9eb
728843-ui <728843@student.dusd.net>
2026-02-24 15:27:47 -0800

    cxbdfbne

--- p7PlayerControlBarragan/Assets/Scripts/PlayerController.cs
+++ p7PlayerControlBarragan/Assets/Scripts/PlayerController.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float turnSpeed = 45.0f;
-    private float speed = 20.0f;
-    private float horizontalInput;
-    private float fowardInput;
+    public float rotationSpeed;
+    public float speed;
+    public float verticalInput;
+    
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,11 +18,13 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        fowardInput = Input.GetAxis("Vertical");
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("VerticaL");
 
-        //We'll move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * fowardInput);
-        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed);
+
+        //tilt this plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
     }
 }
